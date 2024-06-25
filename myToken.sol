9ac01f17d2290bd9f0da4309da8fd93184ee1166
Aarju Pal <110419438+AarZoooo@users.noreply.github.com>
2024-06-25 12:23:40 +0530

    Create myToken.sol

---
+++ myToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    string _name = "Shell";
+    string _abv = "SHL";
+    address private owner;
+
+
+    constructor(uint initialSupply) ERC20(_name, _abv) {
+
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+
+        owner = msg.sender;
+    }
+
+
+    function mint(address to, uint256 amount) public {
+        if (msg.sender != owner) {
+            revert("Only owner can mint tokens.");
+        }
+
+        _mint(to, amount);
+    }
+
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
